7fa28e0b31c9805eb59f4d2f8a4a58ecf6f33a4d
VE_Shubham <shubhampaliwal@virtualemployee.com>
2019-01-03 20:37:24 +0530

    +1

---
+++ 03. Node.js Fundamentals (Note App)-20190103T095307Z-001/notes-node/app.js
@@ -0,0 +1,27 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+// const os = require('os');
+const _ = require('lodash');
+
+const notes = require('./notes.js');
+
+// var filteredArray = _.uniq(['Mike', 1, 'Shubh', 1, 2, 3, 2, 4]);
+// console.log(filteredArray);
+
+// console.log(_.isString(true));
+// console.log(_.isString('true'));
+
+
+// console.log('Square: ', notes.square(3));
+
+// var res = notes.add(12, 3);
+// console.log(res);
+
+// var res = notes.addNote();
+// console.log(res);
+
+// var user = os.userInfo();
+// console.log(user);
+
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
---
+++ 03. Node.js Fundamentals (Note App)-20190103T095307Z-001/notes-node/notes.js
@@ -0,0 +1,21 @@
+console.log('Starting notes.js');
+
+// console.log(module);
+
+// module.exports.age = 25;
+
+module.exports.addNote = () => {
+	console.log('addNote');
+	return 'New note';
+};
+
+// module.exports.add = (a, b) => {
+// 	console.log('Add Function');
+// 	sum = a + b;
+// 	return sum;
+// };
+
+// module.exports.square = (var_a) => {
+// 	console.log('Square Function');
+// 	return var_a**2;
+// };
