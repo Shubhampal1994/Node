6d650c262f86a20ff15dda8ea90d52ac4786e60f
VE_Shubham <shubhampaliwal@virtualemployee.com>
2019-01-11 17:52:03 +0530

    added express.js

--- 05. Web Servers and Application Deployment-20190103T101524Z-001/node-web-server/server.js
+++ 05. Web Servers and Application Deployment-20190103T101524Z-001/node-web-server/server.js
@@ -1,20 +1,62 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to log that request')
+		}
+	});
+	next();
+});
+
+// app.use((req, res, next) => {
+// 	res.render('maintenance');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-	// response.send('<h1>Hello Express!</h1>');
-	res.send({
-		name: 'Shubham',
-		likes: [
-			'Gymming',
-			'Running'
-		]
+	// res.send('<h1>Hello Express!</h1>');
+
+	// res.send({
+	// 	name: 'Shubham',
+	// 	likes: [
+	// 		'Gymming',
+	// 		'Running'
+	// 	]
+	// });
+
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to Home Page!'
+		// currentYear: new Date().getFullYear()
 	});
 });
 
 app.get('/about', (req, res) => {
-	res.send('About Page');
+	// res.send('About Page');
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+		// currentYear: new Date().getFullYear()
+	});
 });
 
 app.get('/bad', (req, res) => {
@@ -23,4 +65,6 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});
